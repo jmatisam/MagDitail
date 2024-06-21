83b615b8fee251798ec0b909ff92b62c3b66a35d
jmat732 <jmatisamuel@gmail.com>
2024-06-21 20:52:27 +0200

    proyect star

---
+++ src/main/java/jmat_f5bosco/mag_ditail/MagDitailApplication.java
@@ -0,0 +1,13 @@
+package jmat_f5bosco.mag_ditail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagDitailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MagDitailApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jmat_f5bosco/mag_ditail/MagDitailApplicationTests.java
@@ -0,0 +1,13 @@
+package jmat_f5bosco.mag_ditail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MagDitailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
